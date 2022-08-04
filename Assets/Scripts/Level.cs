33f878e5ebfbf937623cf0a907c0d355399ba85a
Bovshis Stas <stanislav.bovshis@gmail.com>
2022-08-05 00:00:00 +0300

    player and level scripts
    but not all

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,9 +1,89 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace DefaultNamespace
 {
     public class Level : MonoBehaviour
     {
-        
+        [SerializeField] private int roadLenght = 15;
+        [SerializeField] private float minDamageOffset = 2;
+        [SerializeField] private float maxDamageOffset = 5;
+
+        [SerializeField] private float roadPartLenght = 5f;
+        [SerializeField] private float roadPartWidth = 6f; // длинна и ширина дороги 
+
+        [SerializeField] private GameObject roadPartPrefab;
+        [SerializeField] private GameObject damagePrefab;
+        [SerializeField] private GameObject finishPrefab;
+        [SerializeField] private GameObject playerPrefab;
+
+        //   private PlayerController _player;
+        //  public PlayerController Player => _player;
+        public PlayerController Player { get; private set; } // тоже самое что и выше
+
+        public void Generatelevel()
+        {
+            Clear();
+            GenerateRoad();
+            GenerateDamage();
+            GeneratePlayer();
+        }
+
+        private void Clear()
+        {
+            Player = null;
+            for (int i = 0; i < transform.childCount)
+        }
+
+        private void GenerateRoad()
+        {
+            Vector3 roadLocalPosition = Vector3.zero;// здание обьекста в 0 позиции 
+            for (int i = 0; i < roadLenght; i++)
+            {
+               GameObject roadPart = Instantiate(roadPartPrefab, transform);
+               roadPart.transform.localPosition = roadLocalPosition;
+               roadLocalPosition.z += roadPartLenght; // доб длинну 
+               
+
+            }
+
+            GameObject finish = Instantiate(finishPrefab, transform);
+            finish.transform.localPosition = roadLocalPosition;
+        }
+
+        private void GenerateDamage()
+        {
+            float fullLenght = roadLenght * roadPartLenght;
+            float currentLenght = roadPartLenght * 2;
+            float damageoffsetX = roadPartWidth / 3;
+            float startPosX = roadPartWidth * 0.5f;
+            
+            while (currentLenght< fullLenght)
+            {
+                float zOffset = Random.Range(minDamageOffset, maxDamageOffset) + minDamageOffset;
+                currentLenght += zOffset;
+                currentLenght = Mathf.Clamp(currentLenght, 0f, fullLenght);
+
+                int damagePosition = Random.Range(0,3);
+                float damagePosX = -startPosX + damageoffsetX * damagePosition;
+                GameObject damage = Instantiate(damagePrefab, transform);
+                Vector3 localPosition = Vector3.zero;
+                localPosition.x = damagePosX;
+                localPosition.y = currentLenght;
+                damage.transform.localPosition = localPosition;
+                
+
+
+            }
+        }
+
+        private void GeneratePlayer()
+        {
+            GameObject player = Instantiate(playerPrefab, transform);
+            player.transform.localPosition = new Vector3(0, 0, roadPartLenght * 0.5f);
+            
+            Player = player.GetComponent<PlayerController>();
+            
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,19 @@ namespace DefaultNamespace
 {
     public class PlayerController : MonoBehaviour
     {
+        private const string RUN = "Run";
+        private const string FAIL = "Fail";
+        private const string DANCE = "Dance ";
+        private static readonly int Run = Animator
+        private static readonly int
+        private static readonly int
         [SerializeField] private float roadWidth = 3;
         [SerializeField] private float forwardSpeed = 5f;
         [SerializeField] private InputHandler inputHandler;
-
+        [SerializeField] private float turnRotationAngle = 20;
+        [SerializeField] private float  lerpSpeed = 5 ;
+        [SerializeField] private Transform view;
+        
         private bool _isActive;
 
         private void Start()
@@ -27,11 +36,17 @@ namespace DefaultNamespace
         private void Move()
         {
             //horizontal move
-            float offset = inputHandler.HorizontalAxis;
+            float offset = inputHandler.HorizontalAxis * roadWidth;
             Vector3 position = transform.localPosition;
             position.x += offset;
             float roadDelta = roadWidth * 0.5f;
             position.x = Mathf.Clamp(position.x, -roadDelta, roadDelta);
+
+            Vector3 rotation = view.localRotation.eulerAngles;
+            rotation.y = Mathf.LerpAngle(rotation.y, offset != 0 ? Mathf.Sign(offset) * turnRotationAngle: 0,)
+                lerpSpeed * Time.deltaTime;
+            view.localRotation = Quaternion.Euler(rotation);
+            
             transform.localPosition = position;
             //fps == 60 dt = 0.01666 // 0.01666 * 60 == 1
             //fps == 120 dt == 0.008333; // 0.008333 * 120 == 1
