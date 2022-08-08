d52fcdd4f2a204afded246059ce261ba5562d30b
Pavel Nikitin <nikitinps92@gmail.com>
2022-08-08 21:46:32 +0300

    Add DestroyWall effects + fix bugs

--- Assets/scripts/DestroyableWall.cs
+++ Assets/scripts/DestroyableWall.cs
@@ -6,14 +6,32 @@ using UnityEngine;
 public class DestroyableWall : MonoBehaviour
 {
     [SerializeField] private GameObject effectPrefab;
-    
+    [SerializeField] private GameObject cubes;
+
+    private bool _isActive = false;
+    private float _timer = 2f;
+    private void Update()
+    {
+        if (_isActive)
+        {
+            _timer -= Time.deltaTime;
+            if (_timer<0f)
+            {
+                cubes.SetActive(false);
+                effectPrefab.SetActive(false);
+                _isActive = false;
+            }
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Instantiate(effectPrefab,transform);
-            Destroy(gameObject);
+            gameObject.SetActive(false);
+            effectPrefab.SetActive(true);
+            cubes.SetActive(true);
+            _isActive = true;
         }
     }
 }
